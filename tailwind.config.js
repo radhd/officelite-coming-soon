bca1164f63d8e34b7b3e36aed0e80958214e25dd
Nika M. <merabishvilinikusha@gmail.com>
2024-05-09 23:01:24 +0400

    Sign up page finished, responsives also done

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,11 @@
 module.exports = {
   content: ["./starter-code/*.{html,js}"],
   theme: {
+    screens: {
+      sm: "375px",
+      md: "768px",
+      xl: "1440px",
+    },
     extend: {
       boxShadow: {
         custom: "0px 50px 50px -25px rgba(75, 92, 154, 0.25)",
