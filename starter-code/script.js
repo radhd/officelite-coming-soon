2a2b73f05c69562416815b80da0fb01adc437449
Nika M. <merabishvilinikusha@gmail.com>
2024-05-09 10:25:57 +0400

    Finished customized Drop-down menu

--- starter-code/script.js
+++ starter-code/script.js
@@ -1,6 +1,14 @@
-const form = document.getElementById("form");
 const selectMenu = document.getElementById("selectMenu");
 const notSelected = document.getElementById("not-selected-btns");
+const selectedBtns = document.querySelectorAll(".selected-btn");
+
+selectedBtns.forEach((button) => {
+  button.addEventListener("click", () => {
+    const selectedText = button.querySelector(".selected-text").innerHTML;
+    const choosedOption = document.getElementById("choosedOption");
+    choosedOption.innerHTML = selectedText;
+  });
+});
 
 selectMenu.addEventListener("click", () => {
   notSelected.classList.toggle("hidden");
